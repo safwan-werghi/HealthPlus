3a4180634e38dcbb90a2d8d1c9b13de14171dc72
safwan-werghi <106870378+safwan-werghi@users.noreply.github.com>
2023-04-16 02:55:21 +0100

    Delete altha directory

--- altha/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import doctor  , appointment ,CustomUser
-# Register your models here.
-
-admin.site.register(doctor)
-#admin.site.register(patient)
-admin.site.register(appointment)
-admin.site.register(CustomUser)
